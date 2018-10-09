c3cf36397ab2bd1a2cf4720c5dde15bea41a4bd5
Peeyush Raj <peeyush.raj@gmail.com>
2018-10-09 16:37:57 +0530

    added database functions & content is now fetched from the database

--- js/main.js
+++ js/main.js
@@ -13,37 +13,79 @@ function loadJSON(callback) {
 }
 
 function init() {
-  loadJSON(function(response) {
-    // Parse JSON string into object
-    var data = JSON.parse(response);
-    var db = new PouchDB('wise');
-    var remoteCouch = false;
-    for (var i=0; i<data.length; i++) {
-      addWisdom(data[i]);
-    }
-  });
+  var db = new PouchDB('wise');
+  var remoteCouch = false;
+  initWisdom(db);
 }
 
 // Database methods to get and probably set the wise-list
 
-function addWisdom(wisdom) {
-  var wisdome = {
-    _id: new Date().toISOString(),
-    title: wisdom.text,
-    author: wisdom.author
-  };
-  db.put(todo, function callback(err, result) {
-    if (!err) {
-      console.log('Just became wiser!');
+// initiate the database
+function initWisdom(db) {
+  db.info(function(err, res) { 
+    if (err) { return console.log(err); }
+    if (res.doc_count == 0) {
+      loadJSON(function(response) {
+        var data = JSON.parse(response);
+        for (var i=0; i<data.length; i++) {
+          addWisdom(db, data[i], i);
+        }    
+      })  
     }
+  })
+}
+
+// add data
+function addWisdom(db, wisdom, index) {
+  var wisdom = {
+    _id: index.toString(),
+    wisdom: wisdom
+  };
+  db.put({
+    _id: wisdom._id,
+    _rev: wisdom._rev,
+    data: wisdom
+  }, function(err, response) {
+    if (err) { return console.log(err); }
+    return console.log(response);
   });
 }
 
-function filterWisdom(filter_by) {
-  return new Array();
+// Fetch all the records
+function getAllWisdom(db, callback) {
+  db.allDocs({include_docs: true}, function(err, response) {
+    if (err) { return console.log(err)}
+    if ((callback == 'undefined') || (callback == null)) {
+      return console.log(response);
+    } else {
+      callback(response.rows);
+    }     
+  });
 }
 
+// Fetch all the records that matches the condition
+function filterWisdomByTitle(db, query, callback) {
+  filterWisdom(
+    db, 
+    {'wisdom.title': query.title}, 
+    function (response) {
+      if ((callback == 'undefined') || (callback == null)) {
+        return console.log(response);
+      } else {
+        callback(response.docs);
+      }
+    }
+  )
+}
 
+// 
+function filterWisdom(db, selector, callback) {
+  db.find({
+    selector: selector
+  }, function (err, response) {
+    if (err) { return console.log(err); }
+    callback(response);
+  });
+}
 
 init()
-
